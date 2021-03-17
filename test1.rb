3146b7beb92cb4c63df14b010f970e90bdeb4e13
NowakJR <80201120+NowakJR@users.noreply.github.com>
2021-03-17 18:13:43 +0100

    Add files via upload

---
+++ test1.rb
@@ -0,0 +1,14 @@
+require 'rspec/autorun'
+class Calculator
+  def add(a,b)
+    7
+  end
+  describe Calculator do
+    describe "#add" do
+      it "returns the sum of two numbers" do
+        calculator = Calculator.new
+        expect(calculator.add(5, 2)).to eql(7)
+      end
+    end
+  end
+  end
